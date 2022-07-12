4be654ceb66ee08138912ab5cd1495e829da13a6
ervnjmsdnts <earvinjamesdantes@gmail.com>
2022-07-12 21:57:30 +0800

    Created auth

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,8 +1,39 @@
+const generate = require("../helpers/generate");
+const verify = require("../helpers/verify");
+const User = require("../models/user");
+
 module.exports.loginController = async (req, res) => {
   const { email, password } = req.body;
-  res.send({ message: `Welcome ${email}` });
+
+  const user = await User.findOne({ email });
+
+  if (!user) return res.status(400).send({ error: "User not found" });
+
+  const isPasswordValid = await verify.hash(password, user.password);
+
+  if (!isPasswordValid)
+    return res.status(400).send({ error: "Password is wrong" });
+
+  const token = generate.jwt({ id: user._id });
+
+  return res.status(200).send({ token, message: "Login success" });
 };
 
 module.exports.registerController = async (req, res) => {
-  res.json({ message: "Register Now Mister" });
+  let { email, password } = req.body;
+  const isEmailExist = await User.findOne({ email });
+
+  if (isEmailExist)
+    return res.status(400).send({ error: "Email already exist" });
+
+  password = await generate.hash(password);
+
+  const user = new User({
+    email,
+    password,
+  });
+
+  await user.save();
+
+  return res.status(201).send({ message: "User created" });
 };
---
+++ src/models/user.js
@@ -0,0 +1,29 @@
+const { model, Schema } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = model("User", userSchema);
